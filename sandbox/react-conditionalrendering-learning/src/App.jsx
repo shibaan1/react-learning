4ce97dbb5daeedc845973cf0b4769cd9012e56fb
Shibaan Askari <askarishibaan1@gmail.com>
2026-06-22 15:47:17 +0530

    learn conditional rendering with if, ternary and &&

---
+++ sandbox/react-conditionalrendering-learning/src/App.jsx
@@ -0,0 +1,49 @@
+// conditional rendering is when a certain condition is met then the folloing component will be rendered otherwise not. there are 3-4 ways of doing this 
+
+// 1. using if else 
+
+// 2. using ternary operator ( condition ? if true : if false)
+
+import { useState } from 'react'
+
+import './App.css'
+import LogoutBtn from './components/LogoutBtn'
+import LoginBtn from './components/LoginBtn'
+
+function App() {
+  const [isloggedin , setlogin] = useState(true)
+
+    // if(isloggedin){
+
+    //   return(
+    //     <LogoutBtn />
+      
+    //   )
+    // }
+
+    // else{
+    //  return(
+    //    <LoginBtn />
+    //  )
+    // }
+
+
+//     return(
+
+// //  ternary operator    
+//     <div>
+
+//   {isloggedin ? <LogoutBtn/> : <LoginBtn/>}
+
+// </div>
+// )
+return(
+  <div>
+    <h1>this is conditional rendering</h1>
+    {isloggedin && <LogoutBtn />}
+    {!isloggedin && <LoginBtn/>}
+  </div>
+)
+}
+
+export default App
